9f998b8fc65d0cafdccf93d7067b42955a058fb9
EartH <htrae.back@gmail.com>
2015-06-06 17:27:38 +0700

    After Add Style

--- resources/views/member/edit.blade.php
+++ resources/views/member/edit.blade.php
@@ -1,16 +1,21 @@
+@extends('../admin/template')
+
+@section('admin-content')
 @if(session()->has('message') != '')
 	<h3>Message : {{ session('message') }}</h3>
 @endif
 <form action="{{ route('member.store') }}" method="post">
-	User Name
+	<div class='col-md-1'>Username</div>
 	<input type="text" name="name" value="{{ @$user->name }}">
-	
-	Img
+	<br><br>
+	<div class='col-md-1'>Img</div>
 	<input type="text" name="img" value="{{ @$user->img }}">
-	
-	<input type="submit" name="Edit">
+	<br><br>
+	<div class='col-md-1'></div>
+	<a href="{{ route('member.index') }}" class='btn btn-primary'>Back</a>
+	<input type="submit" class='btn btn-success' name="Edit">
 	
 	<input type="hidden" name="old_id" value="{{ @$user->id }}">
 	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 </form>
-<a href="{{ route('member.index') }}">Back</a>
+@endsection
--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -1,7 +1,10 @@
+@extends('../admin/template')
+
+@section('admin-content')
 @if(session()->has('message') != '')
 	<h3>Message : {{ session('message') }}</h3>
 @endif
-<table border='1'>
+<table class='table'>
 	<thead>
 		<tr>
 			<th>ID</th>
@@ -18,13 +21,14 @@
 				<td>{{ $user->img }}</td>
 				<td>
 					<form action="{{ route('member.destroy', ['id' => $user->id]) }}" method='post'>
-						<a href="{{ route('member.edit', ['id' => $user->id]) }}">Edit</a>
+						<a href="{{ route('member.edit', ['id' => $user->id]) }}" class='btn btn-success'>Edit</a>
 						<input type='hidden' name='_method' value='DELETE'>
 						<input type='hidden' name='_token' value='{{ csrf_token() }}'>
-						<input type='submit' onclick="return confirm('Are you ready ?');" value='Delete'>
+						<input type='submit' class='btn btn-danger' onclick="return confirm('Are you ready ?');" value='Delete'>
 					</form>
 				</td>
 			</tr>
 		@endforeach
 	</tbody>
 </table>
+@endsection
