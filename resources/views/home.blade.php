10e5c4aaab9858ee17c4499e7c796f3d32733212
Mike Paniwan <gm_master-mike@hotmail.com>
2015-06-06 17:33:31 +0700

    create Homepage (use HomeController)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,14 +10,9 @@
 				<div class="panel-body">
 					@foreach($trends as $trend)
 					<div class="media">
-				      <div class="media-left">
-				        <a href="#">
-				          <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjkyMTg3NSIgeT0iMzIiIHN0eWxlPSJmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdDtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj42NHg2NDwvdGV4dD48L2c+PC9zdmc+" data-holder-rendered="true" style="width: 64px; height: 64px;">
-				        </a>
-				      </div>
 				      <div class="media-body">
-				        <h4 class="media-heading" id="top-aligned-media">{{ $trend->getTopic->subject }}<a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4>
-				        <p>{{ $trend->getTopic->description }}</p>
+				        <h4 class="media-heading" id="top-aligned-media"><a href='#'>{{ $trend->getTopic->subject }}</a><a class="anchorjs-link" href="#top-aligned-media"><span class="anchorjs-icon"></span></a></h4>
+				        <p>Posted By <a href='#'>{{ $trend->getTopic->getUser->name }}</a></p>
 				      </div>
 				    </div>
 				    @endforeach
@@ -25,12 +20,18 @@
 			</div>
 		</div>
 		<div class="col-md-4">
+			@foreach($categories as $category)
 			<div class="list-group">
 			  <a href="#" class="list-group-item">
-			    <h4 class="list-group-item-heading">List group item heading</h4>
-			    <p class="list-group-item-text">...</p>
+			    <h4 class="list-group-item-heading">{{ $category->name }}</h4>
+			    <p class="list-group-item-text">
+			    	@foreach($category->getTags as $tag)
+			    	{{ $tag->name }} 
+			    	@endforeach
+			    </p>
 			  </a>
 			</div>
+			@endforeach
 		</div>
 	</div>
 </div>
